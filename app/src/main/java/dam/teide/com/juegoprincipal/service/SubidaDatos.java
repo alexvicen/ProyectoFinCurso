165d91f1e8ee3c1d274de04d30581f1a01f34b95
aes-multimedia <soporte@aes-multimedia.com>
2016-05-05 12:26:36 +0200

    alejandro

---
+++ app/src/main/java/dam/teide/com/juegoprincipal/service/SubidaDatos.java
@@ -0,0 +1,16 @@
+package dam.teide.com.juegoprincipal.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class SubidaDatos extends Service {
+    public SubidaDatos() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
